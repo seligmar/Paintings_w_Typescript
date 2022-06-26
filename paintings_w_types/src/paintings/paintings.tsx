1464e232bb44904550cd7665de4363ed5dcc9ea3
github username <github email address>
2022-06-26 12:48:56 +0100

    added paintings

--- paintings_w_types/src/paintings/paintings.tsx
+++ paintings_w_types/src/paintings/paintings.tsx
@@ -1,5 +1,13 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react'
+import paintings from '../paintings'
 
 export default function Paintings () {
-  return <div>paintings</div>
+  const [allPaintings, setPaintings] = useState<any[]>([])
+
+  var importedPaintings: any[] = paintings
+  useEffect(() => setPaintings(importedPaintings))
+
+  console.log(allPaintings)
+
+  return (<div>paintings</div>)
 };
