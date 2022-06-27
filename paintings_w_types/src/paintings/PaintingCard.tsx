5dda1273189477fd95b6422305b8459f6376522b
github username <github email address>
2022-06-27 18:04:03 +0100

    added info to painting cards

--- paintings_w_types/src/paintings/PaintingCard.tsx
+++ paintings_w_types/src/paintings/PaintingCard.tsx
@@ -2,11 +2,17 @@ import React from 'react'
 
 export default function PaintingCard (painting: any) {
   var paintingForCard = painting.painting
-  console.log(paintingForCard.image)
+  console.log(paintingForCard)
   return (
     <div>
-      This
-      <img src={paintingForCard.image} />
+      <div className='card-image'>
+        <img src={paintingForCard.image} />
+      </div>
+      <p>Artist: {paintingForCard.artist.name}</p>
+      <p>
+        Title: <i>{paintingForCard.title}</i>
+      </p>
+      <p>Date: {paintingForCard.date}</p>
     </div>
   )
 }
